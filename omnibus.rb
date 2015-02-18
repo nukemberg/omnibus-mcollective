e72006954b8bf84e2420bda9908113800a323d44
Avishai Ish-Shalom <avishai@fewbytes.com>
2015-02-19 00:35:46 +0200

    Use git cache

--- omnibus.rb
+++ omnibus.rb
@@ -3,4 +3,5 @@ build_dir "/tmp/omnibus/build"
 package_dir "/tmp/omnibus/pkg"
 source_dir "/tmp/omnibus/src"
 base_dir "./build"
-use_git_caching false
+use_git_caching true
+git_cache_dir "/tmp/omnibus/git_cache"
